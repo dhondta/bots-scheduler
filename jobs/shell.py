2c3b1a7f46fdb4d67de8fc1bc6343ea64aea7411
dhondta <alexandre.dhondt@gmail.com>
2020-05-28 17:45:34 +0200

    Improved job.shell

--- jobs/shell.py
+++ jobs/shell.py
@@ -2,19 +2,19 @@
 import codecs
 from subprocess import check_output, PIPE
 
-from .base import *
+from ._base import *
 
 
 class Shell(JobBase):
     info = {
-        'notes': "This will run an executable program. You can specify as many "
-                 "arguments as you want. This job will pass these arguments to "
-                 "the program in order.",
-        'arguments': [{'type': 'string', 'description': 'Executable path'}],
+        'job_class_name':    "Shell Command",
+        'notes':             "This will run an executable program. You can specify a list of arguments.",
+        'arguments':         [{'type': 'string', 'description': 'Executable path'}],
         'example_arguments': '["/usr/local/my_program", "--file", "/tmp/abc"]',
     }
     
     @report
     def run(self, *args, **kwargs):
         out = check_output(args, stderr=PIPE)
-        return "## `%s`" % " ".join(args), Code(codecs.decode(out, "utf-8"))
+        return Section(" ".join(args)), Code(codecs.decode(out, "utf-8"))
+
