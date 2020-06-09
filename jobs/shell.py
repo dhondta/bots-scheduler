7b2661f7d2b9c67f05eaa6feee975fee2b141fb7
dhondta <alexandre.dhondt@gmail.com>
2020-06-09 22:42:13 +0200

    Improved job.shell (2)

--- jobs/shell.py
+++ jobs/shell.py
@@ -5,18 +5,33 @@ from subprocess import check_output, PIPE
 from ._base import *
 
 
-class Shell(JobBase):
+class ShellCommand(JobBase):
     info = {
         'job_class_name':    "Shell Command",
-        'notes':             "This will run an executable program. You can specify a list of arguments.",
-        'arguments':         [{'type': 'string', 'description': 'Executable path'}],
+        'notes':             "This will run an OS command. The command is specified as a list of tokens. "
+                             "Use at your own risk !",
+        'arguments':         [{'type': 'list', 'description': 'tokenized command'}],
         'example_arguments': '["/usr/local/my_program", "--file", "/tmp/abc"]',
-        'enabled':           False,  # enable at own risk
+        'enabled':           False,
     }
     
     @report
     def run(self, *args, **kwargs):
-        cmd = args[0]
-        out = check_output(cmd, stderr=PIPE, shell=True)
+        out = check_output(args, stderr=PIPE)
+        return Section(cmd), Code(codecs.decode(out, "utf-8"))
+
+
+class RawShellCommand(JobBase):
+    info = {
+        'job_class_name':    "Raw Shell Command",
+        'notes':             "This will run an OS command. Use at your own risk !",
+        'arguments':         [{'type': 'string', 'description': 'Executable path'}],
+        'example_arguments': '["/usr/local/my_program", "--file", "/tmp/abc"]',
+        'enabled':           False,
+    }
+    
+    @report
+    def run(self, *args, **kwargs):
+        out = check_output(args[0], stderr=PIPE, shell=True)
         return Section(cmd), Code(codecs.decode(out, "utf-8"))
 
