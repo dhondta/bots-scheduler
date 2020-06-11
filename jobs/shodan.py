0523222727bcab6d72b15f56555882b09cf78830
dhondta <alexandre.dhondt@gmail.com>
2020-06-11 19:54:50 +0200

    Added Shodan hosts check job

--- jobs/shodan.py
+++ jobs/shodan.py
@@ -4,15 +4,22 @@ from pybots import ShodanBot
 from ._base import *
 
 
-class ShodanJob(JobBase):
+class ShodanHostsCheckJob(JobBase):
     info = {
-        'notes': "This will perform a search on Shodan. You can specify as many arguments as you want. "
-                 "This job will pass these arguments to the program in order.",
-        'arguments': [{'type': 'string', 'description': 'Executable path'}],
-        'example_arguments': '["/usr/local/my_program", "--file", "/tmp/abc"]',
+        'job_class_name': "Shodan - Hosts and open services check",
+        'notes': "This will perform a search on Shodan. You must specify an API key and a file with a list of IP "
+                 "addresses or networks (in CIDR notation) to check for.",
+        'arguments': [{'type': 'string', 'description': 'Shodan API key'},
+                      {'type': 'string', 'description': 'Path to the list of IP addresses or networks'}],
+        'example_arguments': '["API_KEY", "/path/to/ips.list"]',
     }
 
     @report
-    def run(self, *args, **kwargs):
+    def run(self, api_key, ips_path, **kwargs):
+        with ShodanBot(api_key) as bot:
+            result = bot.hosts_from_file(ips_path)
+        hosts = OrderedDict()
+        for host, data in sorted(result.items(), key=lambda x: ip2int(x[0])):
+            hosts[host] = data
+        return [Section("Hosts found on Shodan"), Data(hosts)]
 
-        return "## `%s`" % " ".join(args), Code(codecs.decode(out, "utf-8"))
