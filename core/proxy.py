94b23377a7d6c144b621cea1331edf25dfe6c04b
dhondta <alexandre.dhondt@gmail.com>
2020-07-23 16:19:12 +0200

    Minor changes

--- core/proxy.py
+++ core/proxy.py
@@ -37,30 +37,30 @@ def run_proxy(namespace):
 
     :param namespace: options' namespace
     """
-    _ = namespace
+    ns = namespace
     # configure and run the reverse proxy
     mitmlogger = logging.getLogger("mitmproxy.reverse.proxy")
     # 1. set the IP according to the chosen mode
-    host = ["0.0.0.0", "127.0.0.1"][_.local or _.debug]
+    host = ["0.0.0.0", "127.0.0.1"][ns.local or ns.debug]
     # 2. if no htpasswd file provided, create one with the namespace._default_users
-    if not _.htpasswd:
+    if not ns.htpasswd:
         mitmlogger.debug("Generating the htpasswd file...")
         import bcrypt
-        _.htpasswd = ".htpasswd"
-        with open(_.htpasswd, 'wb') as f:
+        ns.htpasswd = ".htpasswd"
+        with open(ns.htpasswd, 'wb') as f:
             for user in namespace.default_users:
                 username, password = user.split(":")
                 salt = bcrypt.gensalt()
                 f.write(b("%s:%s") % (b(username), bcrypt.hashpw(b(password), salt)))
     # 3. setup the arguments for the proxy run function
     args = "--listen-host %s -p %d --proxyauth @%s --mode reverse:http://localhost:%d/" % \
-           (host, _.port, _.htpasswd, _.port + 1)
-    if _.certificate:
-        args += " --certs %s" % _.certificate
+           (host, ns.port, ns.htpasswd, ns.port + 1)
+    if ns.certificate:
+        args += " --certs %s" % ns.certificate
     # 4. now start the server as a separate process
-    mitmlogger.info("Running proxy at {}:{} ...".format(host, _.port))
+    mitmlogger.info("Running proxy at {}:{} ...".format(host, ns.port))
     p = Process(target=run, args=(AuthMaster, cmdline.mitmproxy, args.split()))
     p.start()
-    mitmlogger.info("*** You can access scheduler web ui at https://{}:{} ***".format(host, _.port))
+    mitmlogger.info("*** You can access scheduler web ui at https://{}:{} ***".format(host, ns.port))
     return p
 
