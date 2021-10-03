c7a72c6ffab42cfca47ec531038e36158ced0681
dhondta <alexandre.dhondt@gmail.com>
2021-10-03 20:18:43 +0200

    Added Censys host enumeration bot

--- jobs/censys.py
+++ jobs/censys.py
@@ -22,3 +22,23 @@ class CensysHostsCheckJob(JobBase):
             hosts[host] = data
         return [Section("Hosts found on Censys"), Data(hosts)]
 
+
+class CensysHostsEnumeration(JobBase):
+    info = {
+        'job_class_name': "Censys - Hosts enumeration from available certificates",
+        'notes':          "This will perform a search on Censys for hostnames contained in public certificates. You "
+                          "must specify API credentials (ID and secret) and a file with a list of domain names.",
+        'arguments':      [{'type': 'string', 'description': 'Censys API identifier'},
+                           {'type': 'string', 'description': 'Censys API secret'},
+                           {'type': 'file', 'description': 'Path to the list of domain names'}],
+    }
+
+    @report
+    def run(self, api_id, api_secret, domains_path, **kwargs):
+        with CensysBot(api_id, api_secret) as bot:
+            self._data = bot.hosts_from_domains_file(domains_path)
+        hosts = OrderedDict()
+        for host, data in sorted(self._data.items(), key=lambda x: x[0]):
+            hosts[host] = data
+        return [Section("Hosts found from certificates via Censys"), Data(hosts)]
+
